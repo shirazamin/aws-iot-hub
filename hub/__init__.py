f3ae332ed1fa7f16a37e69155c7bedd016c8e1ce
Shiraz Amin <shiraz@shirazamin.com>
2018-12-16 19:31:49 -0500

    updated gitignore for certs

--- hub/__init__.py
+++ hub/__init__.py
@@ -1,1 +1,27 @@
 from core import hmm
+import json
+
+with open('../config.json', 'r') as f:
+    config = json.load(f)
+
+# Import SDK packages
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+# For certificate based connection
+myMQTTClient = AWSIoTMQTTClient("myClientID")
+# For Websocket connection
+# myMQTTClient = AWSIoTMQTTClient("myClientID", useWebsocket=True)
+# Configurations
+# For TLS mutual authentication
+myMQTTClient.configureEndpoint("YOUR.ENDPOINT", 8883)
+# For Websocket
+# myMQTTClient.configureEndpoint("YOUR.ENDPOINT", 443)
+# For TLS mutual authentication with TLS ALPN extension
+# myMQTTClient.configureEndpoint("YOUR.ENDPOINT", 443)
+myMQTTClient.configureCredentials("YOUR/ROOT/CA/PATH", "PRIVATE/KEY/PATH", "CERTIFICATE/PATH")
+# For Websocket, we only need to configure the root CA
+# myMQTTClient.configureCredentials("YOUR/ROOT/CA/PATH")
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
